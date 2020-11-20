e0bf51c16d8e88e952672b707f90a4f50600bcc0
azzarkani hanane <azzarkani.hanane@gmail.com>
2020-11-20 09:20:42 +0100

    done

--- js/index.js
+++ js/index.js
@@ -1,23 +1,43 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-
+  console.log("Calculating subtotal, yey");
+  const price = product.querySelector(".price span");
+  const quantity = product.querySelector(".quantity input");
+  const unitPrice = price.innerHTML;
+  const val = quantity.value;
+  const subT = unitPrice * val;
+  const subTotalPrice = product.querySelector(".subtotal");
+  subTotalPrice.innerHTML = "$" + subT;
+  
+  console.log(subT);
+  console.log(subTotalPrice);
+  
+  // console.log("hanane",unitPrice);
+ 
   //... your code goes here
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+ 
   // end of test
+  const lesProduits = document.querySelector(".product");
+  let sum = 0;
+  lesProduits.forEach((produit) => {
+    // console.log(produit);
+    let update = updateSubtotal(produit);
+    let toTal = sum + update;
+    return toTal;
+  });
+
 
   // ITERATION 2
-  //... your code goes here
+  //je n'ai pas réussis à faire cette partie j'ai essayé d'appliquer le for eac
 
   // ITERATION 3
-  //... your code goes here
+  //
 }
 
 // ITERATION 4
